eefa56c357945116cb0a0a50bed239d542aa8b06
GrabovyiS <ecclesiasth@gmail.com>
2022-08-14 15:58:07 +0300

    counter

--- main.js
+++ main.js
@@ -43,9 +43,7 @@ buttonContainer.addEventListener("click", () => {
     postLight();
     orb.classList.add("glow");
     +light.innerHTML++
-    // getCount();
     setTimeout(() => {
         orb.classList.remove("glow");
     }, 3000);
-    getCount();
 });
